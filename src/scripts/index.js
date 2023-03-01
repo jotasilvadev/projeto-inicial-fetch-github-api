cf425764c9e2b8d2e33b155fdd2645c91bb157b2
João Paulo da Silva <jotapesilva1990@gmail.com>
2023-03-01 13:40:07 -0300

    aula-final

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,13 @@
-import { user } from "/src/scripts/services/user.js"
-import { repositories } from "/src/scripts/services/repositories.js";
+import { getUser } from "/src/scripts/services/user.js";
+import { getRepositories } from "/src/scripts/services/repositories.js";
+
+import { user } from "/src/scripts/objects/user.js";
+import { screen } from "/src/scripts/objects/screen.js";
 
 document.getElementById("btn-search").addEventListener("click", () => {
     const userName = document.getElementById("input-search").value;
-    getUserProfile(userName);
+    if (validateEmptyInput(userName)) return;
+    getUserData(userName);
 });
 
 document.getElementById("input-search").addEventListener("keyup", (e) => {
@@ -12,39 +16,30 @@ document.getElementById("input-search").addEventListener("keyup", (e) => {
     const isEnterKeyPressed = key === 13;
 
     if (isEnterKeyPressed) {
-        getUserProfile(userName);
+        if (validateEmptyInput(userName)) return;
+        getUserData(userName);
     }
 });
 
-function getUserProfile(userName) {
-    user(userName).then((userData) => {
-        let userInfo = `<div class="info">
-        <img src="${userData.avatar_url}" alt="Foto do perfil do usuário" />
-        <div class="data">
-            <h1>${userData.name ?? "Não possui nome cadastrado 😒"}</h1>
-            <p>${userData.bio ?? "Não possui bio cadastrada 🤨"}</p>
-        </div>
-        </div>`;
-
-        document.querySelector(".profile-data").innerHTML = userInfo;
-
-        getUserRepositories(userName);
-    });
+function validateEmptyInput(userName) {
+    if (userName.length === 0) {
+        alert("Preencha o campo com o nome do usuário do Github");
+        return true;
+    }
 }
 
-function getUserRepositories(userName) {
-    repositories(userName).then((reposData) => {
-        let repositoriesItens = "";
+async function getUserData(userName) {
+    const userResponse = await getUser(userName);
 
-        reposData.forEach((repo) => {
-            repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</li>`;
-        });
+    if(userResponse.message === "Not found"){
+        screen.renderNotFound()
+        return
+    }
 
-        document.querySelector(
-            ".profile-data"
-        ).innerHTML += `<div class="repositories section"
-    <h2>Repositórios</h2>
-    <ul>${repositoriesItens}</ul>
-    </div>`;
-    });
+    const repositoriesResponse = await getRepositories(userName);
+    user.setInfo(userResponse);
+    user.setRepositories(repositoriesResponse);
+
+    console.log(user);
+    screen.renderUser(user);
 }
